9c56a270064b56fcf074b908ba76fba77d5f098c
白 空 <2316664125@qq.com>
2025-05-01 22:19:07 +0800

    习惯打卡部分构建5.1

--- app/src/main/java/com/aiden/desine/activities/Home_activity.java
+++ app/src/main/java/com/aiden/desine/activities/Home_activity.java
@@ -1,16 +1,44 @@
 package com.aiden.desine.activities;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.aiden.desine.R;
 
+import com.aiden.desine.fragments.HabitFragment;
+
+
 public class Home_activity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_habit) {
+                selectedFragment = new HabitFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
+
+        // 设置默认显示的Fragment
+        if (savedInstanceState == null) {
+            bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        }
     }
 }
